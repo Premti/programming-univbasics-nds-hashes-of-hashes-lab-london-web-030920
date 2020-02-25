c45e2fc291fe37a1475f400887de3e761d4fb613
premti <premti@gmail.com>
2020-02-25 16:07:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,6 +41,7 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
+  new_hash = 
   
-return HoH
+return new_hash
 end
