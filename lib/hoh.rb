26c189d005a9efbc175b4967aa0040a323349826
premti <premti@gmail.com>
2020-02-25 16:13:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,9 +55,7 @@ def naming_system
              label: "Genus",
              sub_category:{
                label: "Species",
-               sub_category:{
-                 label: "Species"
-               }
+               sub_category:{}
              }
            }
          }
